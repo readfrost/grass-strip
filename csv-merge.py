1e95b7a1fbddda1a93fbc7ed8bd8fc21f1751080
readfrost <read_frost@yahoo.com>
2024-05-17 11:58:19 -0400

    Update csv-merge.py

--- csv-merge.py
+++ csv-merge.py
@@ -7,8 +7,8 @@ This program reads two csv files and merges them based on a common key column.
 import pandas as pd
 
 # Read the files into two dataframes.
-df1 = pd.read_csv('CSV1.csv')
-df2 = pd.read_csv('CSV2.csv')
+df1 = pd.read_csv('APT_BASE.csv')
+df2 = pd.read_csv('APT_RWY.csv')
 
 # Merge the two dataframes, using _ID column as key
 df3 = pd.merge(df1, df2, on = 'ID_')
